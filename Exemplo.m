2ec317d8375a22074a986f062cba7877ccc14f6f
Eduardo Mendes <eduardo.mendes@engenharia.ufjf.br>
2023-08-15 17:07:26 -0300

    histograma de nrand e R

--- Exemplo.m
+++ Exemplo.m
@@ -1,38 +1,37 @@
 clc
 clear
+close all
 
 % Monte Carlo
-E= 0.02;
-V=12;
-R_inf= 2*(1-E); 
-R_sup= 2*(1+E);
-n_s=1000000;
-minimo = inf;
-maximo = -inf;
+E = 0.02;
+V = 12;
+R_inf = 2*(1-E); 
+R_sup = 2*(1+E);
+n_s = 1000000;
+nrand = zeros(1,n_s);
 
-for k=1:n_s
-nrand = rand;
-if nrand < minimo
-   minimo = nrand;
-end
-if nrand > maximo
-   maximo = nrand;
-end
+for k = 1:n_s
+nrand(k) = rand;
 
-R(:,k)= R_inf +(R_sup - R_inf)*nrand;
-I(:,k)= V/R(k);
+R(:,k) = R_inf +(R_sup - R_inf)*nrand(k);
+I(:,k) = V/R(k);
 end
 
 
-I_inf=min(I);
-I_max=max(I);
-I_MC=[I_inf I_max];
-figure(1)
+I_inf = min(I);
+I_max = max(I);
+I_MC = [I_inf I_max];
+figure
 boxplot(I);
 
-figure(2)
+figure
 hist(I)
 
+figure
+hist(R)
+
+figure
+hist(nrand)
 
 % % AI (Matematica Intervalar)
 % R=infsup(1.96,2.04);
