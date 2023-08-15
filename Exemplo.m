eab392c6bfb7ac90f0d5355d76c62b7f4d0d7088
Eduardo Mendes <eduardo.mendes@engenharia.ufjf.br>
2023-08-15 17:00:23 -0300

    limites min e max do rand - monte carlo

--- Exemplo.m
+++ Exemplo.m
@@ -1,16 +1,25 @@
 clc
-clear all
+clear
 
 % Monte Carlo
 E= 0.02;
 V=12;
 R_inf= 2*(1-E); 
 R_sup= 2*(1+E);
-n_s=1000;
+n_s=1000000;
+minimo = inf;
+maximo = -inf;
 
 for k=1:n_s
+nrand = rand;
+if nrand < minimo
+   minimo = nrand;
+end
+if nrand > maximo
+   maximo = nrand;
+end
 
-R(:,k)= R_inf +(R_sup - R_inf)*rand;
+R(:,k)= R_inf +(R_sup - R_inf)*nrand;
 I(:,k)= V/R(k);
 end
 
@@ -25,22 +34,22 @@ figure(2)
 hist(I)
 
 
-% AI (Matematica Intervalar)
-R=infsup(1.96,2.04);
-I_AI=V/R;
-
-
-
-disp('----------------------------------')
-disp('        Corrente Intervalar ')
-disp('----------------------------------')
-fprintf('%s'   , '| MC')
-fprintf('%s'    ,'|  ');
-fprintf('[%4.4f; %4.4f]' ,I_MC);
-fprintf('%s \n' ,' |  ');
-fprintf('%s'   , '| AI')
-fprintf('%s'    ,'|  ');
-fprintf('[%4.4f; %4.4f]' ,[inf(I_AI) sup(I_AI)]);
-fprintf('%s \n' ,' |  ');
-disp('---------------------------------')
+% % AI (Matematica Intervalar)
+% R=infsup(1.96,2.04);
+% I_AI=V/R;
+% 
+% 
+% 
+% disp('----------------------------------')
+% disp('        Corrente Intervalar ')
+% disp('----------------------------------')
+% fprintf('%s'   , '| MC')
+% fprintf('%s'    ,'|  ');
+% fprintf('[%4.4f; %4.4f]' ,I_MC);
+% fprintf('%s \n' ,' |  ');
+% fprintf('%s'   , '| AI')
+% fprintf('%s'    ,'|  ');
+% fprintf('[%4.4f; %4.4f]' ,[inf(I_AI) sup(I_AI)]);
+% fprintf('%s \n' ,' |  ');
+% disp('---------------------------------')
 
