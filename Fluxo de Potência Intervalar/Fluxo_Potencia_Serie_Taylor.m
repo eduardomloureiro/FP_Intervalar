0c4791ad71f62a1ecb4259536f7e2513f520251f
Eduardo Mendes <eduardo.mendes@engenharia.ufjf.br>
2023-08-18 13:39:06 -0300

    mod 2

--- Fluxo de Potência Intervalar/Fluxo_Potencia_Serie_Taylor.m
+++ Fluxo de Potência Intervalar/Fluxo_Potencia_Serie_Taylor.m
@@ -15,11 +15,11 @@ Jacob;
 % Incerteza
 incerteza = 0.05;
 
-% Dados da Rede Elétrica 
+% Dados da Rede Elétrica
 
 %                 Barra   V     A   Pg     Qg    Pl     Ql    ysht_bus tipo  Qmin Qmax
                
-DADOS_BARRA= [      1    1      0    0     0     0     0       0        2     0    0
+DADOS_BARRA = [     1    1      0    0     0     0     0       0        2     0    0
                     2    1      0    0     0    20.0   5.0     0        0     0    0
                     3    1      0    0     0    20.0   5.0     0        0     0    0
                     4    1      0    0     0    20.0   5.0     0        0     0    0
@@ -39,7 +39,7 @@ DADOS_BARRA= [      1    1      0    0     0     0     0       0        2     0 
 
 %                  B_DE    B_PARA  r%   x%  tap    ang. tap  ysht_linha  tp
             
-DADOS_REDE= [        1         2     2   5    0           0       0    1
+DADOS_REDE = [       1         2     2   5    0           0       0    1
                      2         3     2   5    0           0       0    1
                      3         4     2   5    0           0       0    1
                      4         5     2   5    0           0       0    1
@@ -54,7 +54,7 @@ DADOS_REDE= [        1         2     2   5    0           0       0    1
                      5         13    2   5    0           0       0    1
                      13        14    2   5    0           0       0    1
                      ];
-                    
+
 % Incerteza associada as cargas
 
 P_incert = -(DADOS_BARRA(:,6)*(1-incerteza));
@@ -65,25 +65,25 @@ Q_incert = -(DADOS_BARRA(:,7)*(1-incerteza));
 derivada_prim = inv(Jacob)*([P_incert;Q_incert]);
 
 % % Atualização das cargas na matriz DADOS_BARRA
-% 
+%
 % DADOS_BARRA(:,6) = DADOS_BARRA(:,6)*(1-incerteza);
 % DADOS_BARRA(:,7) = DADOS_BARRA(:,7)*(1-incerteza);
-% 
+%
 % delta = abs(incerteza)*ones(length(DADOS_BARRA(:,6))*2,1);
 
-% 
-        incerteza = -incerteza;         % Incerteza superior no algoritmo
-        
-        V_int = solucao_V + derivada_prim(1:nb); % Vetor de solução de primeira ordem  
-        Ang_int = solucao_Ang + derivada_prim(nb+1:end);
-                
-        incerteza = incerteza;         % Incerteza inferior no algoritmo
-        Vsol= solucao_V + derivada_prim(1:nb); % Vetor de solução de primeira ordem  
-        Int(:,2)= Vsol;     
+%
+incerteza = -incerteza;         % Incerteza superior no algoritmo
+
+V_int = solucao_V + derivada_prim(1:nb); % Vetor de solução de primeira ordem
+Ang_int = solucao_Ang + derivada_prim(nb+1:end);
+
+incerteza = incerteza;         % Incerteza inferior no algoritmo
+Vsol = solucao_V + derivada_prim(1:nb); % Vetor de solução de primeira ordem
+Int(:,2) = Vsol;
 
 %Int.V = [min(Int(:,2)')' max(Int(:,2)')'];
-Int_V.V = [min(([Int(:,2)])')' max(([Int_V.V])')'];
-Int.Ang = [min([Int(:,2)]')' max([Int(:,2)]')'];
+Int_V = min(([Int(:,2))')' max(([Int_V.V])')';
+Int_Ang = min([Int(:,2)]')' max([Int(:,2)]')';
 
 toc; % finalização
 
